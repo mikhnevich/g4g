e53feedab9a430827cd5295ae254d013710ba894
fessa <phessa@gmail.com>
2014-03-19 12:57:42 -0400

    'tree'

---
+++ tree/Node.java
@@ -0,0 +1,20 @@
+package tree;
+
+/**
+ * Created by sm84878 @ 3/18/14 4:52 PM
+ */
+public class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public Node(int value, Node left, Node right) {
+        this(value);
+        this.left = left;
+        this.right = right;
+    }
+}

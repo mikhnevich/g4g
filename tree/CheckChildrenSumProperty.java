3b8168816d54703ca8fe03adf5f6b188fdd4eaca
fessa <phessa@gmail.com>
2014-03-25 15:22:14 -0400

    children sum property

---
+++ tree/CheckChildrenSumProperty.java
@@ -0,0 +1,48 @@
+package tree;
+
+
+import util.Pair;
+
+public class CheckChildrenSumProperty {
+
+    public static boolean check(Node root) {
+        return checkInternal(root).getLeft();
+    }
+
+
+    private static Pair<Boolean, Integer> checkInternal(Node root) {
+        if (root == null) {
+            return new Pair<>(true, 0);
+        }
+        if (root.left == null && root.right == null) {
+            return new Pair<>(true, root.value);
+        }
+        Pair<Boolean, Integer> left = checkInternal(root.left);
+        if (!left.getLeft()) {
+            return new Pair<>(false, 0);
+        }
+        Pair<Boolean, Integer> right = checkInternal(root.right);
+        if (!right.getLeft()) {
+            return new Pair<>(false, 0);
+        }
+        final int childrenSum = left.getRight() + right.getRight();
+        final boolean sumProperty = childrenSum == root.value;
+        return new Pair<>(sumProperty, root.value);
+    }
+
+
+
+    public static void main(String[] args) {
+        Node r3 = new Node(10,
+                new Node(8,
+                        new Node(3),
+                        new Node(5)),
+                new Node(2,
+                        new Node(2),
+                        null
+                )
+        );
+        BST_Utils.printNode(r3);
+        System.out.println(check(r3));
+    }
+}

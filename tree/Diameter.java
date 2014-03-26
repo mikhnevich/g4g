ab6ad0d8d1ce29c14c4a29361ece498436965fe4
mikhnevich <mikhnevich@gmail.com>
2014-03-26 07:29:00 -0400

    diameter

--- tree/Diameter.java
+++ tree/Diameter.java
@@ -5,26 +5,66 @@ package tree;
  */
 public class Diameter {
     public static int diameter(Node root) {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+        int left = diameter(root.left);
+        int right = diameter(root.right);
+        return left + right
+    }
+
+    public static Pair<Integer, Integer> diameter(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = diameter(root.left);
+        int right = diameter(root.right);
+        return left + right
     }
 
     public static void main(String[] args) {
 
-
-        Node r3 = new Node(10,
-                new Node(8,
-                        new Node(3),
-                        new Node(5)),
+        Node r = new Node(1,
                 new Node(2,
-                        new Node(2),
-                        new Node(1)
-                )
-        );
+                        new Node(4,
+                                new Node(8,
+                                        new Node(16),
+                                        new Node(17)),
+                                new Node(9,
+                                        new Node(18),
+                                        new Node(19))
+                        ),
+                        new Node(5,
+                                new Node(10,
+                                        new Node(20),
+                                        new Node(21)),
+                                new Node(11,
+                                        new Node(22),
+                                        new Node(23))
+                        )
+                ),
+                new Node(3,
+                        new Node(6,
+                                new Node(12,
+                                        new Node(24),
+                                        new Node(25)),
+                                new Node(13,
+                                        new Node(26),
+                                        new Node(27))
+                        ),
+                        new Node(7,
+                                new Node(14,
+                                        new Node(28),
+                                        new Node(29)),
+                                new Node(15,
+                                        new Node(30),
+                                        new Node(31))
+                        )
+                ));
 
+        BST_Utils.printNode(r);
 
-        BST_Utils.printNode(r3);
-
-        int d = diameter(r3);
+        int d = diameter(r);
         System.out.println(d);
     }
 }

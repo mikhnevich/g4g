775c9ef254e753960ffca612958cf12d7fddba7d
fessa <phessa@gmail.com>
2014-03-26 15:56:32 -0400

    tree diameter

--- tree/Diameter.java
+++ tree/Diameter.java
@@ -1,70 +1,90 @@
 package tree;
 
+import util.Pair;
+
 /**
- * Created on 3/25/2014.
+ * http://www.geeksforgeeks.org/diameter-of-a-binary-tree/
+ * The diameter of a tree (sometimes called the width) is the number of nodes on the longest path between two leaves in the tree. The diagram below shows two trees each with diameter nine, the leaves that form the ends of a longest path are shaded (note that there is more than one path in each tree of length nine, but no path longer than nine nodes).
  */
 public class Diameter {
     public static int diameter(Node root) {
-        if (root == null) {
-            return 0;
-        }
-        int left = diameter(root.left);
-        int right = diameter(root.right);
-        return left + right
+        return diameterInternal(root).getRight();
     }
 
-    public static Pair<Integer, Integer> diameter(Node root) {
+    public static Pair<Integer, Integer> diameterInternal(Node root) {
         if (root == null) {
-            return 0;
+            return new Pair<>(0, 0);
         }
-        int left = diameter(root.left);
-        int right = diameter(root.right);
-        return left + right
+        Pair<Integer, Integer> left = diameterInternal(root.left);
+        Pair<Integer, Integer> right = diameterInternal(root.right);
+        final Integer leftPath = left.getLeft();
+        final Integer rightPath = right.getLeft();
+        final Integer leftDiameter = left.getRight();
+        final Integer rightDiameter = right.getRight();
+
+        int maxPath = Math.max(leftPath, rightPath);
+        int maxDiameter = Math.max(leftDiameter, rightDiameter);
+        int currentDiameter = leftPath + rightPath + 1;
+        return new Pair<>(maxPath + 1, Math.max(currentDiameter, maxDiameter));
     }
 
     public static void main(String[] args) {
 
         Node r = new Node(1,
                 new Node(2,
-                        new Node(4,
-                                new Node(8,
-                                        new Node(16),
-                                        new Node(17)),
-                                new Node(9,
-                                        new Node(18),
-                                        new Node(19))
-                        ),
+                        new Node(4),
                         new Node(5,
-                                new Node(10,
-                                        new Node(20),
-                                        new Node(21)),
-                                new Node(11,
-                                        new Node(22),
-                                        new Node(23))
+                                new Node(7),
+                                new Node(8)
                         )
                 ),
                 new Node(3,
+                        null,
                         new Node(6,
-                                new Node(12,
-                                        new Node(24),
-                                        new Node(25)),
-                                new Node(13,
-                                        new Node(26),
-                                        new Node(27))
-                        ),
-                        new Node(7,
-                                new Node(14,
-                                        new Node(28),
-                                        new Node(29)),
-                                new Node(15,
-                                        new Node(30),
-                                        new Node(31))
+                                null,
+                                new Node(9,
+                                        new Node(10,
+                                                new Node(12),
+                                                new Node(13)),
+                                        new Node(11)
+                                )
                         )
-                ));
+                )
+        );
+
+        Node r2 = new Node(1,
+                new Node(2,
+                        new Node(4,
+                                new Node(7),
+                                new Node(8,
+                                        new Node(10,
+                                                new Node(13),
+                                                new Node(14)),
+                                        null
+                                )
+                        ),
+                        new Node(5,
+                                null,
+                                new Node(9,
+                                        new Node(11),
+                                        new Node(12,
+                                                null,
+                                                new Node(15))
+                                )
+                        )
+                ),
+                new Node(3,
+                        null,
+                        new Node(6)
+                )
+        );
 
         BST_Utils.printNode(r);
 
         int d = diameter(r);
         System.out.println(d);
+
+        int d2 = diameter(r2);
+        System.out.println(d2);
     }
 }

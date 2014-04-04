cc620832ddaefbfdc75f91392383e6e4dab86634
fessa <phessa@gmail.com>
2014-04-04 14:38:04 -0400

    intial commit

---
+++ python/FindPeak.py
@@ -0,0 +1,1 @@
+#  Given an array in which elements are first increasing and then decreasing. find the maximum element in the array.
---
+++ python/MinDifference.py
@@ -0,0 +1,8 @@
+#  Given an array of unsorted elements, find the minimum difference between any 2 elements in the array.
+
+# 1. sort, then iterate
+#  Time:  O(n*log(n))
+#  space: O(1)
+#
+# 2.
+

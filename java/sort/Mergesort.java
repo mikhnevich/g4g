42d6786ded505ad5cddffa053db4b9b89af41018
name <phessa@gmail.com>
2014-05-15 16:34:07 -0400

    mergesort

--- java/sort/Mergesort.java
+++ java/sort/Mergesort.java
@@ -22,15 +22,13 @@ public class Mergesort {
     }
 
     private void merge(int[] array, int[] helper, int lo, int mid, int hi) {
-        for (int i = lo; i < hi; i++) {
-            helper[i] = array[i];
-        }
+        System.arraycopy(array, lo, helper, lo, hi + 1 - lo);
 
         int hL = lo;
         int hR = mid + 1;
         int current = lo;
 
-        while (hL < mid && hR < hi) {
+        while (hL <= mid && hR <= hi) {
             if (helper[hL] <= helper[hR]) {
                 array[current] = helper[hL];
                 hL++;
@@ -40,15 +38,15 @@ public class Mergesort {
             }
             current++;
         }
-        int remaining = mid - hL;
-        for (int i = 0; i < remaining; i++) {
-            array[current + i] = helper[hL + 1];
+        while (hL <= mid) {
+            array[current] = helper[hL];
+            hL++;
+            current++;
         }
-
     }
 
     public static void main(String[] args) {
-        final int[] a = {1, 3, 2, 5};
+        final int[] a = {2, 3, 1};
         new Mergesort().mergesort(a);
         System.out.println(Arrays.toString(a));
     }
